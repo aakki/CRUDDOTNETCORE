681643044c0244ffef548e61be029d7307fc8d31
AnilVaghasiya <anil.vaghasiya2014@gmail.com>
2020-04-02 15:44:25 +0530

    Add project files.

---
+++ CRUDDOTNETCOREAPP/Models/CMSContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDDOTNETCOREAPP.Models
+{
+    public class CMSContext : DbContext
+    {
+        public CMSContext(DbContextOptions<CMSContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
